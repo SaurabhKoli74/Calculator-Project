b99cdcc0bf7ffd0ac15edf8a029175780d85796d
Saurabh <saurabh.koli7420@gmail.com>
2023-05-10 15:45:21 +0530

    Initial Commit & Implemented index.html style.css. Basic operations are implemented in Script.js like +, -, /, * but need to implement remaining features to implement

---
+++ script.js
@@ -0,0 +1,52 @@
+var display = document.getElementById('display');
+var btns = document.querySelectorAll('.button-container div');
+
+
+//var needs to be declared glabally in order keep track of previous values
+var op1 = null;
+var operator = null;
+var op2 = null;
+let res = null;
+function calc() {
+    //'this' contains the element on which the handler is called
+    // console.log(this.getAttribute('data-value'));
+    var temp = this.getAttribute('data-value');
+    if (temp === '+' || temp === '-' || temp === '/' || temp === '*') {
+        if (operator === null) {
+            if (display.innerHTML !== '') {
+                operator = temp;
+                op1 = display.innerText;
+                // console.log(op1, operator);
+                display.innerHTML = '';
+            }
+
+        }
+    
+    } else if (temp === '=') {
+
+        if (operator !== null && display.innerText!=='') {
+            // console.log('inside')
+            op2 = display.innerText;
+
+            res = eval(op1 + " " + operator + " " + op2);
+            // console.log(res);
+            
+            operator=null;
+            display.innerHTML = res;
+        }
+    } else if(temp=='c'){
+        display.innerHTML='';
+        op1=null;
+        op2=null;
+        operator=null;
+        
+    }else {
+
+        display.innerText += temp;
+    }
+}
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', calc);
+}
+

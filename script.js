a9a683dcf6866977f351ca5261d82c05ecaadb55
Saurabh <saurabh.koli7420@gmail.com>
2023-05-17 09:53:41 +0530

    Fixed bug

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function calc() {
     // console.log(this.getAttribute('data-value'));
     var temp = this.getAttribute('data-value');
 
-    if(temp==='back' || temp ==='ce'  || temp==='back' || temp==='^' || temp==='sc' || temp==='' || temp==='%' || temp==='1/x'){
+    if (temp === 'back' || temp === 'ce' || temp === 'back' || temp === '^' || temp === 'sc' || temp === '' || temp === '%' || temp === '1/x') {
         return;
     }
     if (temp === '+' || temp === '-' || temp === '/' || temp === '*') {
@@ -24,27 +24,35 @@ function calc() {
                 display.innerHTML = '';
             }
 
+        } else if (display.innerHTML !== '') {
+            op2=display.innerHTML;
+            res = eval(op1+operator+op2);
+            operator = temp;
+            op1 = res;
+            op2 = null;
+            display.innerHTML=res;
+            
         }
-    
+
     } else if (temp === '=') {
 
-        if (operator !== null && display.innerText!=='') {
+        if (operator !== null && display.innerText !== '') {
             // console.log('inside')
             op2 = display.innerText;
 
             res = eval(op1 + " " + operator + " " + op2);
             // console.log(res);
-            
-            operator=null;
+
+            operator = null;
             display.innerHTML = res;
         }
-    } else if(temp=='c'){
-        display.innerHTML='';
-        op1=null;
-        op2=null;
-        operator=null;
-        
-    }else {
+    } else if (temp == 'c') {
+        display.innerHTML = '';
+        op1 = null;
+        op2 = null;
+        operator = null;
+
+    } else {
 
         display.innerText += temp;
     }
